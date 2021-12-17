c07ddd9eca58e7c827f97495f50e180303ea610b
ghnr <45798027+ghnr@users.noreply.github.com>
2021-12-17 18:49:18 +0000

    Implemented leaderboard for word list

--- highscoringwords.py
+++ highscoringwords.py
@@ -1,4 +1,14 @@
-__author__ = 'codesse'
+from dataclasses import dataclass
+from operator import attrgetter
+
+
+@dataclass
+class ScrabbleWord:
+    word: str
+    scrabble_value: int
+
+    def __lt__(self, other):
+        return (-self.scrabble_value, self.word) > (-other.scrabble_value, other.word)
 
 
 class HighScoringWords:
@@ -7,33 +17,48 @@ class HighScoringWords:
     letter_values = {}
     valid_words = []
 
-    def __init__(self, validwords='wordlist.txt', lettervalues='letterValues.txt'):
+    def __init__(self, valid_words='wordlist.txt', letter_values='letterValues.txt'):
         """
         Initialise the class with complete set of valid words and letter values by parsing text files containing the data
-        :param validwords: a text file containing the complete set of valid words, one word per line
-        :param lettervalues: a text file containing the score for each letter in the format letter:score one per line
+        :param valid_words: a text file containing the complete set of valid words, one word per line
+        :param letter_values: a text file containing the score for each letter in the format letter:score one per line
         :return:
         """
-        with open(validwords) as f:
+        with open(valid_words) as f:
             self.valid_words = f.read().splitlines()
 
-        with open(lettervalues) as f:
+        with open(letter_values) as f:
             for line in f:
                 (key, val) = line.split(':')
                 self.letter_values[str(key).strip().lower()] = int(val)
 
-    def build_leaderboard_for_word_list(self):
+    def build_leaderboard_for_word_list(self) -> list:
         """
-        Build a leaderboard of the top scoring MAX_LEADERBOAD_LENGTH words from the complete set of valid words.
+        Build a leaderboard of the top scoring MAX_LEADERBOARD_LENGTH words from the complete set of valid words.
         :return: The list of top words.
         """
+        scrabble_words = []
+        for word in self.valid_words:
+            scrabble_value = sum([self.letter_values[letter] for letter in word])
+            scrabble_words.append(ScrabbleWord(word, scrabble_value))
+        
+        sorted_scrabbled_words = sorted(scrabble_words, reverse=True)
+        return sorted_scrabbled_words[:self.MAX_LEADERBOARD_LENGTH]
 
-    def build_leaderboard_for_letters(self, starting_letters):
+    def build_leaderboard_for_letters(self, starting_letters: str) -> list:
         """
-        Build a leaderboard of the top scoring MAX_LEADERBOARD_LENGTH words that can be built using only the letters contained in the starting_letters String.
-        The number of occurrences of a letter in the startingLetters String IS significant. If the starting letters are bulx, the word "bull" is NOT valid.
-        There is only one l in the starting string but bull contains two l characters.
-        Words are ordered in the leaderboard by their score (with the highest score first) and then alphabetically for words which have the same score.
+        Build a leaderboard of the top scoring MAX_LEADERBOARD_LENGTH words that can be built using only the letters
+        contained in the starting_letters String. The number of occurrences of a letter in the startingLetters String
+        IS significant. If the starting letters are bulx, the word "bull" is NOT valid. There is only one l in the
+        starting string but bull contains two l characters. Words are ordered in the leaderboard by their score
+        (with the highest score first) and then alphabetically for words which have the same score.
         :param starting_letters: a random string of letters from which to build words that are valid against the contents of the wordlist.txt file
         :return: The list of top buildable words.
         """
+        return []
+
+
+if __name__ == '__main__':
+    high_scoring_words = HighScoringWords()
+    word_leaderboard = high_scoring_words.build_leaderboard_for_word_list()
+    print(word_leaderboard)
---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+
+from highscoringwords import HighScoringWords
+
+
+class TestHighScoringWords(unittest.TestCase):
+    def setUp(self):
+        pass
